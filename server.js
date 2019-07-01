67469e9fddad0e9603d60c5867d322d676eb2824
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2019-07-01 09:24:05 -0700

    Deploying to Heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
-const items = require('./routes/api/items');
+const path = require("path");
+const items = require("./routes/api/items");
 
 // Init express into app variable
 const app = express();
@@ -11,16 +11,20 @@ const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(bodyParser.json());
-// app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // Routes
-app.use('/api/items/', items);
+app.use("/api/items/", items);
+
+// Serve static assets (client's build folder), if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // DB Config
 const db = require("./config/keys").mongoURI;
